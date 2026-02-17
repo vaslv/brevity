b1f267c66ec29e20002ef76957e556bfb550de26
Andrei Vasilev <vaslv@proton.me>
2026-02-17 18:46:53 +0700

    feat(packages): install Horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,5 @@ return [
     App\Providers\ConditionServiceProvider::class,
     App\Providers\Filament\MainPanelProvider::class,
     App\Providers\HashidsServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
