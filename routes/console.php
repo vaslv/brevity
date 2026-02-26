09943902e708f76ee8f09a510ab7f64fbbabac3d
Andrei Vasilev <vaslv@proton.me>
2026-02-26 17:35:41 +0700

    refactor(routes): schedule Horizon SnapshotCommand

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Laravel\Horizon\Console\SnapshotCommand;
 
 Schedule::call(function () {
     @touch('/tmp/healthy');
 })->everyTenSeconds();
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command(SnapshotCommand::class)->everyFiveMinutes();
